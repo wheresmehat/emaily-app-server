483e10ff2ca12757a2b9bd2d3c7278e4ae9fbac8
wheresmehat <csstogo@gmail.com>
2017-08-23 15:32:00 +0200

    Google oauth added

--- index.js
+++ index.js
@@ -1,11 +1,31 @@
 const express = require("express");
+const passport = require("passport");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+const keys = require("./config/keys");
 
 const app = express();
 
-app.get("/", (req, res) => {
 
-    res.send({ welcome: "to emaily app" });
-});
+passport.use(
+    
+    new GoogleStrategy(
+    
+        {
+
+            clientID: keys.googleClientID,
+            clientSecret: keys.googleClientSecret,
+            callbackURL: "/auth/google/callback"
+
+        }, 
+        
+        (accessToken) => {
+
+            console.log(accessToken);
+        }
+    )
+
+);
 
 const PORT = process.env.PORT || 5000;
 
