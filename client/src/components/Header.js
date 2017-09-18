f1bf5dd35c23f7712e3164050744b9ccd763259b
wheresmehat <csstogo@gmail.com>
2017-09-18 15:54:35 +0200

    added payment with stripe.com using react-stripe-checkout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
+import Payments from "./Payments";
+
 class Header extends Component {
 
     renderContent() {
@@ -15,7 +17,11 @@ class Header extends Component {
                 return <li><a href="/auth/google">Login With Google</a></li>;
 
             default:  
-                return <li><a href="/api/logout">Logout</a></li>;
+                return [
+                
+                    <li><Payments /></li>,
+                    <li><a href="/api/logout">Logout</a></li>
+                ];
         }
 
     }
--- config/prod.js
+++ config/prod.js
@@ -6,7 +6,11 @@ module.exports = {
 
     mongoURI: process.env.MONGO_URI,
 
-    cookieKey: process.env.COOKIE_KEY
+    cookieKey: process.env.COOKIE_KEY,
+
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
 
 
