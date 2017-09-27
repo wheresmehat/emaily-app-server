7f5e290b8198633ad9b75b26f0eea1bda106ad01
wheresmehat <csstogo@gmail.com>
2017-09-27 12:12:48 +0200

    sendgrid setup

--- config/prod.js
+++ config/prod.js
@@ -10,7 +10,9 @@ module.exports = {
 
     stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
 
-    stripeSecretKey: process.env.STRIPE_SECRET_KEY
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+
+    sendGridKey: process.env.SEND_GRID_KEY
 };
 
 
--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const requireLogin = require("../middlewares/requireLogin");
 const requireCredits = require("../middlewares/requireCredits");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/surveyTemplate");
 
 const Survey = mongoose.model("survey");
 
@@ -24,7 +26,7 @@ module.exports = (app) => {
 
         });
 
-        
+        const mailer = new Mailer(survey, surveyTemplate(survey));
 
     });
 
